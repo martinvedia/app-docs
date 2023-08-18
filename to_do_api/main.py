c71a085c2ac9c55ef52a97e646f3f3adb62d1eb6
Martin <martin.vedia@hotmail.com>
2023-08-17 22:48:10 -0300

    create_task con postman

--- to_do_api/main.py
+++ to_do_api/main.py
@@ -9,15 +9,18 @@ def root():
 @app.route("/tasks/<task_id>")
 def get_task(task_id):
     tasks = {'task_id': task_id, 'descrip': 'Una descripción de una task'}
+    query = request.args.get("query")
+
+    if query:
+        tasks["query"] = query
+
     return jsonify(tasks), 200
 
-@app.route('/tasks', methods=['POST'])
-def create_task(descrip):
+@app.route("/tasks", methods=["POST"])
+def create_task():
     data = request.get_json()
-    return jsonify(data), 200
+    data["status"] = "Task created"
+    return jsonify(data), 201
         
-    
-    
-
 if __name__ == '__main__':
     app.run(debug = True)
